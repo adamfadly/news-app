aacd245000f099867267186cb257f1461510d81c
Mocha Adam Fadly <adamfadly11@gmail.com>
2020-12-11 11:28:27 +0700

    typing filter search bar handling error when the value is empty

--- src/store/modules/headlines.js
+++ src/store/modules/headlines.js
@@ -1,48 +1,51 @@
-import {topHeadlines, topHeadlinesByFilter} from "../../services/sourceList"
+import { topHeadlines, topHeadlinesByFilter } from "../../services/sourceList";
 
-export default ({
+export default {
   namespaced: true,
+
   state: {
-		news: [],
-		indexCard: "",
-		visitedPages: []
-	},
-	getters:{
-		getAllSourceList(state){
-			return state.news
-		},
-		
-		getAllVisitedPages(state){
-			return state.visitedPages
-		}
-	},
-	mutations: {
-		HANDLE_CHANGE_NEWS(state,payload) {
-			state.news = payload
-		},
-		HANDLE_CHANGED_INDEXCARD(state,payload) {
-			state.indexCard = payload
-		},
-		HANDLE_CHANGED_VALUETITLE(state,payload) {
-			state.news[state.indexCard].title = payload
-		},
+    news: [],
+    indexCard: "",
+    visitedPages: [],
+  },
 
-		HANDLE_CHANGED_VISITED(state,payload) {
-			state.visitedPages.push(payload)
-		}
-	},
-	actions: {
-		async headlinesList({commit}) {
-			let payload = await topHeadlines()
-			commit("HANDLE_CHANGE_NEWS", payload)
-		},
+  getters: {
+    getAllSourceList(state) {
+      return state.news;
+    },
 
-		async getHeadlinesByTyping ({commit}, payload) {
-			let value = await topHeadlinesByFilter(payload)
-			commit("HANDLE_CHANGE_NEWS", value)
-		}
-	},
-});
+    getAllVisitedPages(state) {
+      return state.visitedPages;
+    },
+  },
 
+  mutations: {
+    HANDLE_CHANGE_NEWS(state, payload) {
+      state.news = payload;
+    },
 
-// getHeadlinesByTyping
+    HANDLE_CHANGED_INDEXCARD(state, payload) {
+      state.indexCard = payload;
+    },
+
+    HANDLE_CHANGED_VALUETITLE(state, payload) {
+      state.news[state.indexCard].title = payload;
+    },
+
+    HANDLE_CHANGED_VISITED(state, payload) {
+      state.visitedPages.push(payload);
+    },
+  },
+
+  actions: {
+    async headlinesList({ commit }) {
+      let payload = await topHeadlines();
+      commit("HANDLE_CHANGE_NEWS", payload);
+    },
+
+    async getHeadlinesByTyping({ commit }, payload) {
+      let value = await topHeadlinesByFilter(payload);
+      commit("HANDLE_CHANGE_NEWS", value);
+    },
+  },
+};
